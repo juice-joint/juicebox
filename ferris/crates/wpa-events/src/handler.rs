17f479041d20e18798967da85ca08a28116bb827
jared-paul <jaredpaul9820@gmail.com>
2025-07-16 22:20:35 -0700

    remove wifi-handshake and wpa-events crates, lets make them separate repos

--- ferris/crates/wpa-events/src/handler.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-use async_trait::async_trait;
-use crate::event::WpaEvent;
-
-#[async_trait]
-pub trait WpaEventHandler: Send + Sync {
-    /// Handle a wpa_supplicant event
-    async fn handle_event(&self, event: WpaEvent) -> Result<()>;
-}
--- ferris/crates/wpa-events/src/lib.rs
+++
@@ -1,8 +0,0 @@
-
-pub mod event;
-pub mod monitor;
-pub mod handler;
-
-pub use event::{WpaEvent, WpaState};
-pub use monitor::WpaEventMonitor;
-pub use handler::WpaEventHandler;
